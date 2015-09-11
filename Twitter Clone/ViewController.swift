a6f5c22b9d8d29adeac28a51f74ddbec0542cc71
vanhensy <anhvynv.vanhensy@gmail.com>
2015-09-12 02:50:42 +0700

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Nguyễn Vương Anh Vỹ on 9/12/15.
+//  Copyright (c) 2015 Nguyễn Vương Anh Vỹ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
